5151f3e83199e1f0c414385b7c5c8d8da7624812
kong-wep <pakkapol.20100@gmail.com>
2022-09-03 17:05:55 +0700

    Added qrcode

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import {generatePayload} from "../promptpay";
+import {QRCodeSVG} from 'qrcode.react';
 const Home = () =>{
+	let ppID = "0909754062";
+	let amount = 2000;
+	let payload = generatePayload(ppID,amount);
 	return (
+		<>
 		<h1>Home</h1>
+		<QRCodeSVG value={payload} />
+		</>
 	)
 }
 export default Home;
